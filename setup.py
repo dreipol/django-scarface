4d476dde03bfe21a77551107429f36349297a45a
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2015-01-06 10:53:02 +0100

    new Pypi version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-scarface',
-    version='1.0',
+    version='1.0.1',
     packages=['scarface'],
     include_package_data=True,
     license='MIT License',
@@ -23,11 +23,12 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License', # example license
+        'License :: OSI Approved :: MIT License', 
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
