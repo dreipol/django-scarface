86b2578136a19c7ee41ed7e80cb1585f656620e5
Jan Meier <jan.meier@dreipol.ch>
2015-12-18 14:47:14 +0100

    updated beta version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-scarface',
-    version='3.0-alpha-3',
+    version='3.0-alpha-4',
     packages=['scarface'],
     include_package_data=True,
     license='MIT License',
