87ca8aecd97d2491b4e1380c19037c92fc4f1474
Jan Meier <jan.meier@dreipol.ch>
2015-12-18 14:21:56 +0100

    added device to admin area

--- scarface/admin.py
+++ scarface/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 
 from scarface.forms import PlatformAdminForm
-from scarface.models import Application, Platform, Topic, PushMessage
+from scarface.models import Application, Platform, Topic, PushMessage, Device
 
 __author__ = 'janmeier'
 
@@ -25,7 +25,12 @@ class TopicAdmin(admin.ModelAdmin):
     list_display = ['name', 'application', 'arn']
 
 
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['device_id', 'platform', 'arn', 'push_token']
+
+
 admin.site.register(Application, ApplicationAdmin)
 admin.site.register(Platform, PlatformAdmin)
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(PushMessage)
+admin.site.register(Device, DeviceAdmin)
