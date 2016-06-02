a44fe45e33e858c76ddaebd860891a6549d5cb5d
philipplaeubli <phil@dreipol.ch>
2016-06-02 10:48:19 +0200

    Release preparation
    - Dropped Python 2.6 support
    - Updated version
    - Updated Changelog

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 import os
+
 from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
-reqs =['boto>=2.34.0',]
+reqs = ['boto>=2.34.0', ]
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-scarface',
-    version='3.0-alpha-6',
+    version='3.0-alpha-7',
     packages=['scarface'],
     include_package_data=True,
     license='MIT License',
@@ -26,7 +27,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
